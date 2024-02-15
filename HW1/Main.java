033f3ef6f0007f552e62ca5e4253ad2aa913e87b
Aramis Hernandez <ah1027406@wcupa.edu>
2024-02-15 09:29:46 -0500

    Update Main.java

---
+++ HW1/Main.java
@@ -0,0 +1,13 @@
+package HW1;
+import java.util.*;
+
+public class Main{
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int index = input.nextInt();
+        
+        int[] date = {};
+
+    }
+}
